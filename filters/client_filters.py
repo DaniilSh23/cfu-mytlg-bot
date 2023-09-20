c35ed0adfb6c2c6dbe387c3fee33abe3280d18ec
DaniilSh23 <danyasevas11@gmail.com>
2023-09-20 12:33:59 +0300

    добавил в фильтр хэндлера новых постов проверку, что у поста есть какой-либо текст

--- filters/client_filters.py
+++ filters/client_filters.py
@@ -1,12 +1,16 @@
 from pyrogram import filters
 
-from settings.config import CLIENT_CHANNELS
+from settings.config import CLIENT_CHANNELS, MY_LOGGER
 
 
 async def func_listening_channel_filter(_, client, update):
     """
     Фильтрация апдейта из канала, который прослушивает аккаунт.
     """
+    if not update.text:
+        MY_LOGGER.warning(f'У поста отсутствует текст, он не будет передан в хэндлер!')
+        return False
+
     channels = CLIENT_CHANNELS[client.acc_pk]
     ch_ids_lst = [str(i_ch.get('channel_id')) for i_ch in channels]
     return update.chat and str(update.chat.id) in ch_ids_lst
